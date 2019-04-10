b1a5ba1c2bb5ba2f054247d84720cd6f6857b3ef
boruch d <dubywider@gmail.com>
2019-04-10 19:18:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,8 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }programmer_hash[:yukihiro_matsumoto] = {
+     }
+     programmer_hash[:yukihiro_matsumoto] = {
 		   :known_for => "Ruby",
 		   :languages => ["LISP, C"]
 		 }
