2a726e3485c7913cbfdc9b2233767aa8b08eb8ca
boruch d <dubywider@gmail.com>
2019-04-10 19:19:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,8 +107,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     
      
      
 end
